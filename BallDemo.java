4079af7647709d7bfed0ae5be4d6898cf0d48a1a
Víctor <victorlopez94leon@gmail.com>
2016-03-17 11:05:03 +0100

    Añadida la posibilidad de crear una bola de tamaño aleatorio

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -26,26 +26,23 @@ public class BallDemo
     public void bounce()
     {
         int ground = 400;   // position of the ground line
-
+        Random random = new Random();
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+        BouncingBall ball = new BouncingBall(50, 50,random.nextInt(250), Color.BLUE, ground, myCanvas);
         ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
 
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
             ball.move();
-            ball2.move();
             // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+            if(ball.getXPosition() >= 550) {
                 finished = true;
             }
         }
